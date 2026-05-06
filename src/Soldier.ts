263cf070cafff8a8429a7a0c5ba6d36af7b91c3f
albert-dev <al96@hotmail.es>
2026-05-06 13:41:10 +0200

    fix: "SOLVED all Soldier.test.ts tests"

--- src/Soldier.ts
+++ src/Soldier.ts
@@ -1,1 +1,14 @@
-export class Soldier {}
+export class Soldier {
+
+    constructor (public health: number, public strength: number){
+    }
+
+    attack(){
+        return this.strength
+    }
+
+    receiveDamage(damage:number){
+        this.health -= damage
+
+    }
+}
