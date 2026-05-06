52466bc7b406d54dfdcbfb0bc4f4f60e1cc942d8
albert-dev <al96@hotmail.es>
2026-05-06 16:16:13 +0200

    fix: "SOLVED all Viking.test.ts tests"

--- src/Viking.ts
+++ src/Viking.ts
@@ -1,1 +1,21 @@
-export class Viking{}
+import {Soldier} from "../src/Soldier";
+export class Viking extends Soldier{
+    constructor(public name: string, health: number, strength: number){
+        super(health, strength)
+    }
+
+    receiveDamage(damage:number){
+        this.health -= damage;
+
+        if (this.health > 0){
+            return `${this.name} has received ${damage} points of damage`
+        }
+        else {
+            return `${this.name} has died in act of combat`
+        }
+    }
+
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+}
