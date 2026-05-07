795d8a1971e6492cab99be6a03487ded9b69ce04
albert-dev <al96@hotmail.es>
2026-05-07 10:21:10 +0200

    fix: "SOLVED all War.test.ts tests"

--- src/War.ts
+++ src/War.ts
@@ -1,1 +1,60 @@
-export class War {}
+import {Viking} from "./Viking";
+import {Saxon} from "./Saxon";
+import { receiveMessageOnPort } from "worker_threads";
+
+
+export class War {
+        public vikingArmy: Viking[]
+        public saxonArmy: Saxon[]
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(viking: Viking): void {
+        this.vikingArmy.push(viking)
+
+    }  
+    
+    addSaxon(saxon: Saxon): void {
+        this.saxonArmy.push(saxon)
+
+    }
+
+    vikingAttack(){
+    const randomViking = this.vikingArmy[0]; 
+    
+    const randomSaxon = this.saxonArmy[0];
+
+    const result = randomSaxon.receiveDamage(randomViking.strength);
+
+    if (randomSaxon.health <= 0) {
+        this.saxonArmy.splice(0, 1);
+    }
+    return result;
+    }
+
+    saxonAttack(){
+    const randomViking = this.vikingArmy[0]; 
+    
+    const randomSaxon = this.saxonArmy[0];
+
+    const result = randomViking.receiveDamage(randomSaxon.strength);
+
+    if (randomViking.health <= 0) {
+        this.vikingArmy.splice(0, 1);
+    }
+    return result;
+    }
+
+    showStatus(){
+        if (this.saxonArmy = []){
+            return "Vikings have won the war of the century!"
+        }
+    }
+
+
+
+
+
+}
